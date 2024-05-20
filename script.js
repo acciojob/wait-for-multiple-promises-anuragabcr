237f926554fdd00aa8773fe6ad7e7dfd45e59767
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-20 12:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+function createRandomPromise() {
+    const randomTime = Math.floor(Math.random() * 3000) + 1000; // Random time between 1 and 3 seconds
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(randomTime); // Resolving with the random time
+        }, randomTime);
+    });
+}
+
+const promises = [];
+for (let i = 0; i < 3; i++) {
+    promises.push(createRandomPromise());
+}
+
+Promise.all(promises)
+    .then((results) => {
+        let tbody = document.getElementById("output")
+		tbody.innerHTML = ''
+		for (let i = 0; i < 4; i++) {
+			if (i==3) {
+				let tr = document.createElement('tr')
+				tr.innerHTML = `
+					<td>Total</td>
+					<td>${(results[0]+results[1]+results[2])/1000}</td>
+				`
+				tbody.appendChild(tr)
+			}
+			else {
+				let tr = document.createElement('tr')
+				tr.innerHTML = `
+					<td>Promise ${i+1}</td>
+					<td>${results[i]/1000}</td>
+				`
+				tbody.appendChild(tr)
+			}
+		}
+    })
+    .catch((error) => {
+        console.error("Error occurred:", error);
+    });
